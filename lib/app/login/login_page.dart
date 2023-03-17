695c2724437ed2cd6a59656e28b3cd751f564d31
Anna Raczyńska <raczynska.annamaria@gmail.com>
2023-03-17 15:52:27 +0100

    Add Login Functionality

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,15 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
 
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 245, 243, 227),
       body: Center(
-        child: Text('Jesteś niezalogowany'),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Hello again!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  )),
+              const SizedBox(height: 30),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 220, 211, 227),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'e-mail',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 220, 211, 227),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(hintText: 'password'),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: emailController.text,
+                      password: passwordController.text,
+                    );
+                  } catch (error) {
+                    print(error);
+                  }
+                },
+                child: Text('Sign in'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
