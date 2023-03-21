969e4bac92318f2233314d212339676c28718fd0
Anna Raczyńska <raczynska.annamaria@gmail.com>
2023-03-21 13:43:45 +0100

    Implement adding restaurants

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+var restaurantName = '';
+var sushiName = '';
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Second'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(hintText: 'restaurant name'),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(hintText: 'sushi name'),
+            onChanged: (newValue) {
+              setState(() {
+                sushiName = newValue;
+              });
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: (() {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantName,
+                'sushi': sushiName,
+                'rating': 3.0,
+              });
+            }),
+            child: Text('Add'),
+            style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF81b29a)),
+          ),
+        ],
+      ),
     );
   }
 }
