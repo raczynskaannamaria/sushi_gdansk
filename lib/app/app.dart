dfa80eea5ea58941a197825f704d4caf523e9a1d
Anna Raczyńska <raczynska.annamaria@gmail.com>
2023-03-29 14:36:25 +0200

    Implement Global Cubit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sushi_gdansk/app/cubit/root_cubit.dart';
 import 'package:sushi_gdansk/app/features/home/home_page.dart';
 import 'package:sushi_gdansk/app/features/login/login_page.dart';
 
@@ -25,14 +26,15 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          final user = snapshot.data;
-          if (user == null) {
-            return LoginPage();
-          }
-          return HomePage(user: user);
-        });
+    return BlocProvider(
+      create: (context) => RootCubit()..start(),
+      child: BlocBuilder<RootCubit, RootState>(builder: (context, state) {
+        final user = state.user;
+        if (user == null) {
+          return LoginPage();
+        }
+        return HomePage(user: user);
+      }),
+    );
   }
 }
